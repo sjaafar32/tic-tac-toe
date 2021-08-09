ac1b0f543b5ad3a795485f282bda21573c2b4a0b
Shadi Jaafar <shadinjaafar@gmail.com>
2021-08-08 21:18:18 -0400

    Completed gameplay logic and most of UI

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 const gameBoardContainer = document.getElementById('game-board-container');
 const resetBtn = document.getElementById('reset-button');
 const submitBtn = document.getElementById('submit-button');
+const winnerDisplay = document.getElementById('winner-display');
+const xBtn = document.getElementById('x-button');
+const winnerText = document.getElementById('winner-text');
 
 let boardArray;
 let newCell; //This variable is global so it can be used within the functions and outside of them too
@@ -10,6 +13,8 @@ let playerTwo;
 
 let playerOneName;
 let playerTwoName;
+
+let counter = 0;
 
 const createPlayer = (name, marker) => {
     const playerName = () => name;
@@ -35,6 +40,7 @@ const gameBoard = (() => {
         for(i=0; i<9; i++) {
             arrayToDelete[i].textContent = '';
         }
+        counter = 0;
     }
     return {createBoard, resetBoard};
 })();
@@ -43,12 +49,16 @@ const gameBoard = (() => {
 
 
 
-resetBtn.addEventListener('click', () => {
+resetBtn.addEventListener('click', reset);
+
+ function reset() {
     turn = 0;
     boardArray = newCell.returnArray(); 
     //need to get the array from the node list in "createCell" since thats where it was created
+    document.getElementById('player-one').value = '';
+    document.getElementById('player-two').value = '';
     gameBoard.resetBoard(boardArray);
- });
+ }
 
 
 
@@ -62,9 +72,17 @@ resetBtn.addEventListener('click', () => {
 
     playerOne = createPlayer(playerOneName, 'X');
     playerTwo = createPlayer(playerTwoName, 'O');
+
+    document.getElementById('player-one').value = '';
+    document.getElementById('player-two').value = '';
 });
 
 
+
+
+xBtn.addEventListener('click', () => {
+    document.documentElement.style.setProperty('--display', 'none');
+});
 
 
 
@@ -76,7 +94,10 @@ const createCell = () => { //This function is called 9 times in the loop above f
 
     const cellButton = () => {
         newCell.addEventListener('click', () => {
-            newCell.textContent = gamePlay();
+            if(newCell.textContent == ''){  //this if statement prevents a user from changing a marker that's already placed
+                newCell.textContent = gamePlay(); 
+                checkWinner();
+            }
         });
     };
 
@@ -95,22 +116,55 @@ const createCell = () => { //This function is called 9 times in the loop above f
 
 gameBoard.createBoard();
 
+const winPositions = [
+    [0,1,2],
+    [3,4,5],
+    [6,7,8],
+    [0,3,6],
+    [1,4,7],
+    [2,5,8],
+    [0,4,8],
+    [2,4,6]
+];
 
-let turn = 0
+
+let turn = 0;
 
 const gamePlay = () => {
     if(turn === 9){return;}
     if(turn % 2 === 0){ turn++; return playerOne.playerMarker();}
     else {turn++; return playerTwo.playerMarker();}
 
-    const winPositions = [
-        [0,1,2],
-        [3,4,5],
-        [6,7,8],
-        [0,3,6],
-        [1,4,7],
-        [2,5,8],
-        [0,4,8],
-        [2,4,6]
-    ]
+    }   
+
+function checkWinner() {
+        boardArray = newCell.returnArray();
+        for(i = 0; i < 8; i++){
+        if(boardArray[winPositions[i][0]].textContent == 'X' &&
+        boardArray[winPositions[i][1]].textContent == 'X' &&
+        boardArray[winPositions[i][2]].textContent == 'X')
+        {winnerText.textContent = playerOne.playerName() + ' Wins!';
+        document.documentElement.style.setProperty('--display', 'block');
+        reset();
+         return;}
+
+        if(boardArray[winPositions[i][0]].textContent == 'O' &&
+        boardArray[winPositions[i][1]].textContent == 'O' &&
+        boardArray[winPositions[i][2]].textContent == 'O')
+        {winnerText.textContent = playerTwo.playerName() + ' Wins!';
+        document.documentElement.style.setProperty('--display', 'block');
+        reset();
+        return;}
+
+        };
+        counter = 0;
+        for(i = 0; i < 9; i++){
+            if(counter === 8){winnerText.textContent = `It's A Draw!`;
+            document.documentElement.style.setProperty('--display', 'block');
+            reset();
+            }
+            if(boardArray[i].textContent !== ''){
+                counter++;
+            }
+        }
 }
